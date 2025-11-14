ce6d6a13fc7d4933bfdaa4e071744c025868157c
J.D. DeVaughn-Brown <jddevaughnbrown@sierracanyonschool.org>
2025-11-14 05:55:55 -0800

    feat: Save plants in storage and list on the home page

--- theme.ts
+++ theme.ts
@@ -1,7 +1,10 @@
 export const theme = {
+  colorBlack: "#000",
   colorGreen: "#29b365",
+  colorGrey: "#808080",
   colorLeafyGreen: "#206a42",
   colorAppleGreen: "#a0d36c",
   colorLimeGreen: "#d0e57e",
   colorWhite: "#fff",
+  colorLightGrey: "#eee",
 };
