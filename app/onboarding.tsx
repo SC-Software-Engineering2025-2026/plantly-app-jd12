0600f3155ebcfe531572eada3d793c5f08072150
J.D. DeVaughn-Brown <jddevaughnbrown@sierracanyonschool.org>
2025-11-13 19:51:19 -0800

    feat: Set up bottom tabs

---
+++ app/onboarding.tsx
@@ -0,0 +1,17 @@
+// import { View, StyleSheet, Button } from "react-native";
+//  import { theme } from "@/theme";
+//  import { useUserStore } from "@/store/userStore";
+//  import { useRouter } from "expo-router";
+// import { PlantlyButton } from "@/components/PlantlyButton";
+
+//  export default function OnboardingScreen() {
+//    const toggleHasOnboarded = useUserStore((store) => store.toggleHasOnboarded);
+// export default function OnboardingScreen() {
+
+//    return (
+//      <View style={styles.container}>
+// -      <Button title="Let me in!" onPress={handlePress} />
+// +      <PlantlyButton title="Let me in!" onPress={handlePress} />
+//      </View>
+//    );
+//  }
