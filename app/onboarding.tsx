3d2d1a90096d5ec8ebace082c6bde2ee836ac95b
J.D. DeVaughn-Brown <jddevaughnbrown@sierracanyonschool.org>
2025-11-13 19:57:06 -0800

    feat: Add onboarding screen

--- app/onboarding.tsx
+++ app/onboarding.tsx
@@ -1,17 +1,22 @@
-// import { View, StyleSheet, Button } from "react-native";
-//  import { theme } from "@/theme";
-//  import { useUserStore } from "@/store/userStore";
-//  import { useRouter } from "expo-router";
-// import { PlantlyButton } from "@/components/PlantlyButton";
+import { Text, View, StyleSheet } from "react-native";
+import { theme } from "@/theme";
 
-//  export default function OnboardingScreen() {
-//    const toggleHasOnboarded = useUserStore((store) => store.toggleHasOnboarded);
-// export default function OnboardingScreen() {
+export default function OnboardingScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Onboarding</Text>
+    </View>
+  );
+}
 
-//    return (
-//      <View style={styles.container}>
-// -      <Button title="Let me in!" onPress={handlePress} />
-// +      <PlantlyButton title="Let me in!" onPress={handlePress} />
-//      </View>
-//    );
-//  }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: theme.colorWhite,
+  },
+  text: {
+    fontSize: 24,
+  },
+});
