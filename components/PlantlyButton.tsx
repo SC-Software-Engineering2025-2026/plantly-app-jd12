1edca610c1be1e892617ee007c0aa79ffa236b0a
J.D. DeVaughn-Brown <jddevaughnbrown@sierracanyonschool.org>
2025-11-13 20:46:11 -0800

    feat: Implement custom pressable button

--- components/PlantlyButton.tsx
+++ components/PlantlyButton.tsx
@@ -1,5 +1,6 @@
 import { theme } from "@/theme";
-import { StyleSheet, Text, Pressable } from "react-native";
+import { StyleSheet, Text, Pressable, Platform } from "react-native";
+import * as Haptics from "expo-haptics";
 
 type Props = {
   title: string;
@@ -7,8 +8,22 @@ type Props = {
 };
 
 export function PlantlyButton({ title, onPress }: Props) {
+  const handlePressed = () => {
+    if (Platform.OS !== "web") {
+      Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
+    }
+    onPress();
+  };
   return (
-    <Pressable onPress={onPress} style={styles.button}>
+    <Pressable
+      onPress={handlePressed}
+      style={(state) => {
+        if (state.pressed) {
+          return [styles.button, styles.buttonPressed];
+        }
+        return styles.button;
+      }}
+    >
       <Text style={styles.text}>{title}</Text>
     </Pressable>
   );
@@ -25,5 +40,8 @@ const styles = StyleSheet.create({
     paddingVertical: 12,
     borderRadius: 6,
     backgroundColor: theme.colorGreen,
+  },
+  buttonPressed: {
+    backgroundColor: theme.colorLeafyGreen,
   },
 });
