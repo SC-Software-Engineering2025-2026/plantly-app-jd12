8770ea59158c453c92b51dc241074de89355e7b7
J.D. DeVaughn-Brown <jddevaughnbrown@sierracanyonschool.org>
2025-11-14 05:43:10 -0800

    feat: Add Header button and modal

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,8 +1,10 @@
-import { Redirect, Tabs } from "expo-router";
+import { Redirect, Tabs, Link } from "expo-router";
 import Entypo from "@expo/vector-icons/Entypo";
 import Feather from "@expo/vector-icons/Feather";
 import { theme } from "../../theme";
 import { useUserStore } from "@/store/userStore";
+import { AntDesign } from "@expo/vector-icons";
+import { Pressable } from "react-native";
 
 export default function Layout() {
   const hasFinishedOnboarding = useUserStore(
@@ -21,8 +23,20 @@ export default function Layout() {
           tabBarIcon: ({ size, color }) => (
             <Entypo name="leaf" size={size} color={color} />
           ),
+          headerRight: () => (
+            <Link href="/new" asChild>
+              <Pressable hitSlop={20} style={{ marginRight: 18 }}>
+                <AntDesign
+                  name="plus-circle"
+                  size={24}
+                  color={theme.colorGreen}
+                />
+              </Pressable>
+            </Link>
+          ),
         }}
       />
+
       <Tabs.Screen
         name="profile"
         options={{
